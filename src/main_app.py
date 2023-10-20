d3436c143e8c015fe08f7edad388297f71b7fe46
nekita <chubarev.nk@phystech.edu>
2023-10-20 22:55:24 +0300

    string realisation

--- src/main_app.py
+++ src/main_app.py
@@ -2,20 +2,29 @@ from diff_match_patch import diff_match_patch
 import typing
 
 
-def file_comparing(file1: typing.BinaryIO, file2: typing.BinaryIO) -> str:
+def file_comparing_str(file1: typing.TextIO, file2: typing.TextIO) -> (float, float):
     content1 = file1.read()
     content2 = file2.read()
-    len_1 = len(content1)
-    len_2 = len(content2)
-    if len_1 < len_2:
-        len_1, len_2 = len_2, len_1
-        content1, content2 = content2, content1
 
     model = diff_match_patch()
 
-    diff = model.diff_main(str(content1), str(content2))
-    return diff
+    diff = model.diff_main(content1, content2)
+    diff = model.diff_main(content1, content2)
+    delete, insert, equal = __compute_fractions_str(diff)
+    return equal / (delete + equal), equal / (insert + equal)
 
 
-def __compute_percent(diff: list[tuple[int, bytes]]):
-    pass
+def __compute_fractions_str(diff: list[tuple[int, str]]) -> (int, int, int):
+    equality_cnt = 0
+    deletion_cnt = 0
+    insertion_cnt = 0
+    for code, bytes_str in diff:
+        cnt = len(bytes_str)
+        if code == 0:
+            equality_cnt += cnt
+        if code == 1:
+            insertion_cnt += cnt
+        if code == -1:
+            deletion_cnt += cnt
+    return deletion_cnt, insertion_cnt, equality_cnt
+
--- src/start.py
+++ src/start.py
@@ -1,6 +1,6 @@
 import main_app
 
 
-f1 = open("f1", "rb")
-f2 = open("f2", "rb")
+f1 = open("ff1", "r")
+f2 = open("ff2", "r")
 print(main_app.file_comparing(f1, f2))
